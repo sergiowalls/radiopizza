6f35a9102021109c7d8a366769d7cf261d0adde2
Sergio <sergio.paredes25@gmail.com>
2016-07-07 16:53:37 +0200

    Working on galeria

---
+++ script/scripts.js
@@ -0,0 +1,62 @@
+var imagenes = [
+"img/pizza1.jpg",
+"img/pizza2.jpg",
+"img/pizza3.jpg",
+"img/pizza4.jpg",
+"img/pizza5.jpg",
+"img/pizza6.jpg",
+"img/pizza7.jpg",
+"img/pizza8.jpg",
+"img/pizza9.jpg",
+"img/pizza10.jpg",
+"img/pizza11.jpg",
+"img/pizza12.jpg",
+"img/pizza13.jpg",
+"img/pizza14.jpg",
+"img/pizza15.jpg"
+];
+
+var imagenActual;
+var miniaturaActual;
+var contenedor;
+
+function init() {
+	console.log("init");
+	imagenActual = 0;
+	miniaturaActual = 0;
+	contenedor = document.getElementById("fotoGrande");
+}
+
+function fotoIzquierda() {
+	if (imagenActual > 0) {
+		--imagenActual;
+		contenedor.style.backgroundImage = "url("+imagenes[imagenActual]+")";
+	}
+}
+
+function fotoDerecha() {
+	if (imagenActual < 7) {
+		++imagenActual;
+		contenedor.style.backgroundImage = "url("+imagenes[imagenActual]+")";
+	}
+}
+
+function tiraIzquierda() {
+	if (imagenActual > 0) {
+		--imagenActual;
+		contenedor.style.backgroundImage = "url("+imagenes[imagenActual]+")";
+	}
+}
+
+function tiraDerecha() {
+	if (imagenActual < 7) {
+		++imagenActual;
+		contenedor.style.backgroundImage = "url("+imagenes[imagenActual]+")";
+	}
+}
+
+function fotoMiniatura(i) {
+
+}
+
+window.onload = init;
