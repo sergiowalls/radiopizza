48d9543b9b90e3ea5e6c6a16afb7542920c53d86
Victor Gonzalez <victorggep@gmail.com>
2016-07-08 08:52:16 +0200

    joc funcionant i galeria funcionant

--- script/scripts.js
+++ script/scripts.js
@@ -1,25 +1,3 @@
-var imagenes = [
-"img/pizza1.jpg",
-"img/pizza2.jpg",
-"img/pizza3.jpg",
-"img/pizza4.jpg",
-"img/pizza5.jpg",
-"img/pizza6.jpg",
-"img/pizza7.jpg",
-"img/pizza8.jpg",
-"img/pizza9.jpg",
-"img/pizza10.jpg",
-"img/pizza11.jpg",
-"img/pizza12.jpg",
-"img/pizza13.jpg",
-"img/pizza14.jpg",
-"img/pizza15.jpg",
-"img/pizza16.jpg",
-"img/pizza17.jpg",
-"img/pizza18.jpg",
-"img/pizza19.jpg",
-"img/pizza20.jpg"
-];
 
 
 var imagenesApple = [
@@ -30,9 +8,6 @@ var imagenesApple = [
 
 var imagenActualApple;
 var imagenRobot = "img/img-androitor/robot.png"
-var imagenActual;
-var miniaturaActual;
-var contenedor;
 var positionTOPa;
 var positionLEFTa;
 var apple;
@@ -40,73 +15,35 @@ var robot;
 var velocitat;
 var vidas;
 var puntuacio;
-var miniaturas;
-var marcador;
+var Microsoft;
+var	positionTOPm;
+var	positionLEFTm;
 
 function init() {
 	console.log("init");
 	apple = document.getElementById("imgApple");
 	$("#imgApple").click(canviarPosicioA);
-	contenedor = document.getElementById("fotoGrande")
-	robot = document.getElementById("imgRobot")
-	imagenActual = 0;
-	miniaturaActual = 0;;
+	contenedor = document.getElementById("fotoGrande");
+	robot = document.getElementById("imgRobot");
+	Microsoft = document.getElementById("imgMircrosoft");
+	$("#imgMircrosoft").click(canviarPosicioM);
 	imagenActualApple = imagenesApple[Math.floor(Math.random()*3)];
 	positionTOPa = Math.floor(Math.random()*90);
 	positionLEFTa = Math.floor(Math.random()*90);
 	positionTOP = Math.floor(Math.random()*70);
 	positionLEFT = Math.floor(Math.random()*70);
+	positionTOPm = Math.floor(Math.random()*80);
+	positionLEFTm = Math.floor(Math.random()*80);
 	apple.style.background = "url("+imagenActualApple+")";
 	apple.style.top = positionTOPa + "%";
 	apple.style.left = positionLEFTa + "%";
 	robot.style.left = positionLEFT + "%";
 	robot.style.top = positionTOP + "%";
+	Microsoft.style.left = positionLEFTm + "%";
+	Microsoft.style.top = positionTOPm + "%";
 	velocitat = 2000;
 	vidas = 3;
 	puntuacio = 0;
-	marcador = 0;
-	contenedor = document.getElementById("fotoGrande");
-	miniaturas = document.getElementsByClassName("miniatura");
-	miniaturas[0].style.opacity = 1;
-}
-
-function fotoIzquierda() {
-	if (imagenActual > 0) {
-		miniaturas[imagenActual].style.opacity = 0.5;
-		--imagenActual;
-		contenedor.style.backgroundImage = "url("+imagenes[imagenActual]+")";
-		miniaturas[imagenActual].style.opacity = 1;
-	}
-}
-
-function fotoDerecha() {
-	if (imagenActual < 19) {
-		miniaturas[imagenActual].style.opacity = 0.5;
-		++imagenActual;
-		contenedor.style.backgroundImage = "url("+imagenes[imagenActual]+")";
-		miniaturas[imagenActual].style.opacity = 1;
-	}
-}
-
-function tiraIzquierda() {
-	if (marcador > 0) {
-		console.log("tiraIzquierda---")
-		--marcador;
-		miniaturas[marcador].style.display = "inline";
-	}
-}
-
-function tiraDerecha() {
-	console.log()
-	if (marcador < 15) {
-		console.log("tiraDerecha--");
-		miniaturas[marcador].style.display = "none";
-		++marcador;
-	}
-}
-
-function fotoMiniatura(i) {
-
 }
 
 function canviarPosicioA(){
@@ -117,6 +54,25 @@ function canviarPosicioA(){
 	apple.style.top = positionTOPa + "%";
 	apple.style.left = positionLEFTa + "%";
 	puntuacio += 100;
+	velocitat = 2000;
+	$("#imgRobot").stop().animate({top: positionTOPa-10 + "%", left: positionLEFTa + "%"},velocitat, function(){
+		vidas = vidas - 1;
+		if (vidas == 0){
+			alert("Game Over. Puntuació: " +puntuacio+ ".");
+		}
+		else{
+			canviarPosicioA();
+		}
+	});
+}
+
+function canviarPosicioM(){
+	puntuacio += 200;
+	velocitat = 3200;
+	positionTOPm = Math.floor(Math.random()*80);
+	positionLEFTm = Math.floor(Math.random()*80);
+	Microsoft.style.top = positionTOPm + "%";
+	Microsoft.style.left = positionLEFTm + "%";
 	$("#imgRobot").stop().animate({top: positionTOPa-10 + "%", left: positionLEFTa + "%"},velocitat, function(){
 		vidas = vidas - 1;
 		if (vidas == 0){
---
+++ script/scripts_galeria.js
@@ -0,0 +1,81 @@
+var imagenes = [
+"img/pizza1.jpg",
+"img/pizza2.jpg",
+"img/pizza3.jpg",
+"img/pizza4.jpg",
+"img/pizza5.jpg",
+"img/pizza6.jpg",
+"img/pizza7.jpg",
+"img/pizza8.jpg",
+"img/pizza9.jpg",
+"img/pizza10.jpg",
+"img/pizza11.jpg",
+"img/pizza12.jpg",
+"img/pizza13.jpg",
+"img/pizza14.jpg",
+"img/pizza15.jpg",
+"img/pizza16.jpg",
+"img/pizza17.jpg",
+"img/pizza18.jpg",
+"img/pizza19.jpg",
+"img/pizza20.jpg"
+];
+
+var imagenActual;
+var miniaturaActual;
+var contenedor;
+var vidas;
+var miniaturas;
+var marcador;
+
+function init() {
+	console.log("init");
+	imagenActual = 0;
+	miniaturaActual = 0;
+	marcador = 0;
+	contenedor = document.getElementById("fotoGrande");
+	miniaturas = document.getElementsByClassName("miniatura");
+	miniaturas[0].style.opacity = 1;
+}
+
+function fotoIzquierda() {
+	if (imagenActual > 0) {
+		miniaturas[imagenActual].style.opacity = 0.5;
+		--imagenActual;
+		contenedor.style.backgroundImage = "url("+imagenes[imagenActual]+")";
+		miniaturas[imagenActual].style.opacity = 1;
+	}
+}
+
+function fotoDerecha() {
+	if (imagenActual < 19) {
+		miniaturas[imagenActual].style.opacity = 0.5;
+		++imagenActual;
+		contenedor.style.backgroundImage = "url("+imagenes[imagenActual]+")";
+		miniaturas[imagenActual].style.opacity = 1;
+	}
+}
+
+function tiraIzquierda() {
+	if (marcador > 0) {
+		console.log("tiraIzquierda---")
+		--marcador;
+		miniaturas[marcador].style.display = "inline";
+	}
+}
+
+function tiraDerecha() {
+	console.log()
+	if (marcador < 15) {
+		console.log("tiraDerecha--");
+		miniaturas[marcador].style.display = "none";
+		++marcador;
+	}
+}
+
+function fotoMiniatura(i) {
+
+}
+
+
+window.onload = init;
